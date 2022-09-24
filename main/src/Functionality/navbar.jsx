92bbbe3e40b4c390909701bb280ef2222fc04f31
MubarekHussen <mubahussen2014@gmail.com>
2022-09-24 08:51:20 +0300

    styling

--- main/src/Functionality/navbar.jsx
+++ main/src/Functionality/navbar.jsx
@@ -3,13 +3,6 @@ import { Link } from "react-router-dom";
 import { Box, Text, Flex } from "rebass";
 const NavBar = () => {
   return (
-    // <nav className="navbar bg-light">
-    //   <div className="container-fluid">
-    //     <Link className="navbar-brand" to={"/"}>
-    //       Employees
-    //     </Link>
-    //   </div>
-    // </nav>
     <Flex fontSize={4} px={3} color="orange" bg="black" alignItems="center">
       <Link to={"/"}>
         <Text p={3} fontWeight="bold" color="orange">
